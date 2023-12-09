41cf5df7130e1a668f0affe0ce153d1e42d303d4
Chaychaitanya786 <153323918+Chaychaitanya786@users.noreply.github.com>
2023-12-09 14:09:19 -0500

    Update AccountListController.java

--- src/main/java/com/challenge/servicechallenge/controllers/AccountListController.java
+++ src/main/java/com/challenge/servicechallenge/controllers/AccountListController.java
@@ -8,17 +8,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+//Used the RestController annotation to indicate that this class is a controller
+//It return values of its methods should be automatically converted to JSON.
+
 @RestController
 public class AccountListController {
 
     @Autowired
     private AccountListService accountListService;
-
+  /**
+     * Handles HTTP GET requests for retrieving all accounts.
+     *
+     * @return ListOfAccounts containing all accounts.
+     */
+    
     @GetMapping("/accounts")
     ListOfAccounts all() {
         return accountListService.getAllAccounts();
     }
-
+    //Handles HTTP GET requests for retrieving an account by its ID.
+    //@param id The ID of the account to retrieve, provided as a request parameter.
+    //@return Account with the specified ID.
+    
     @GetMapping("/account")
     Account getAccountByID(@RequestParam int id) {
         return accountListService.getAccountByID(id);
