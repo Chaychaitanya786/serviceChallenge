8909e16dd92f395ef227732ac985efd477666ef8
Chaychaitanya786 <153323918+Chaychaitanya786@users.noreply.github.com>
2023-12-09 14:22:20 -0500

    Update ExtractFileNotFoundException.java

--- src/main/java/com/challenge/servicechallenge/exceptionHandelling/ExtractFileNotFoundException.java
+++ src/main/java/com/challenge/servicechallenge/exceptionHandelling/ExtractFileNotFoundException.java
@@ -3,6 +3,12 @@ package com.challenge.servicechallenge.exceptionHandelling;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Custom exception class to represent a scenario where the requested file is not found during extraction.
+ * The @ResponseStatus annotation is used to specify the HTTP status code (404 NOT FOUND)
+ * and the reason for the exception when it is thrown.
+ */
+
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "File Requested not found")
 public class ExtractFileNotFoundException extends RuntimeException {
 }
