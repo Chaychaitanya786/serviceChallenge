0e58fbe428bb8dc6e1b3c13deaf1ba41f1c45bd7
Chaychaitanya786 <153323918+Chaychaitanya786@users.noreply.github.com>
2023-12-09 14:02:13 -0500

    Update ApplicationConfig.java

--- src/main/java/com/challenge/servicechallenge/config/ApplicationConfig.java
+++ src/main/java/com/challenge/servicechallenge/config/ApplicationConfig.java
@@ -5,12 +5,20 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
-
+/**
+ * Configuration class for managing application properties related to a service.
+ * Annotated with @Component for automatic detection and registration as a Spring bean.
+ */
 @Component
 @ConfigurationProperties("service")
 @Data
 public class ApplicationConfig {
 
+    /**
+     * HashMap to store key-value pairs related to file configuration.
+     * The keys and values in this map are configured through application properties
+     * prefixed with "service" in the application configuration files.
+     */
     private final HashMap<String, String> fileConfig;
 
 }
