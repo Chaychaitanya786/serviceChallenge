abe2191dbcd47b47499fb5c9bb11bd7236fdf24e
Chaychaitanya786 <153323918+Chaychaitanya786@users.noreply.github.com>
2023-12-09 14:21:35 -0500

    Update AccountNotFoundException.java

--- src/main/java/com/challenge/servicechallenge/exceptionHandelling/AccountNotFoundException.java
+++ src/main/java/com/challenge/servicechallenge/exceptionHandelling/AccountNotFoundException.java
@@ -3,6 +3,12 @@ package com.challenge.servicechallenge.exceptionHandelling;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Custom exception class to represent a scenario where the requested account ID is not found.
+ * The @ResponseStatus annotation is used to specify the HTTP status code (404 NOT FOUND)
+ * and the reason for the exception when it is thrown.
+ */
+
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Account ID Requested not found")
 public class AccountNotFoundException extends RuntimeException {
 }
