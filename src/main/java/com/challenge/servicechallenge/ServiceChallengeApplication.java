141689aeb90d6abd8b6142e0103a1bfed996d046
Chaychaitanya786 <153323918+Chaychaitanya786@users.noreply.github.com>
2023-12-09 14:37:22 -0500

    Update ServiceChallengeApplication.java

--- src/main/java/com/challenge/servicechallenge/ServiceChallengeApplication.java
+++ src/main/java/com/challenge/servicechallenge/ServiceChallengeApplication.java
@@ -4,12 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+/**
+ * Main application class for the ServiceChallenge application.
+ * Uses the @SpringBootApplication annotation for auto-configuration and component scanning.
+ * Enables caching with the @EnableCaching annotation.
+ */
+
 @SpringBootApplication
 @EnableCaching
 public class ServiceChallengeApplication {
 
     public static void main(String[] args) {
-
+       
+        // Start the Spring Boot application.
         SpringApplication.run(ServiceChallengeApplication.class, args);
     }
 
