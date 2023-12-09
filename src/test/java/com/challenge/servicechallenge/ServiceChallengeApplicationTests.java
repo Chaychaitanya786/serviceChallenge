39b8d104953d8f54b9923cf1f488680074a9c754
Chaychaitanya786 <153323918+Chaychaitanya786@users.noreply.github.com>
2023-12-09 14:00:11 -0500

    Update ServiceChallengeApplicationTests.java

--- src/test/java/com/challenge/servicechallenge/ServiceChallengeApplicationTests.java
+++ src/test/java/com/challenge/servicechallenge/ServiceChallengeApplicationTests.java
@@ -2,12 +2,19 @@ package com.challenge.servicechallenge;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+/**
+ * Integration test class for the ServiceChallengeApplication.
+ * Used the SpringBootTest annotation to configure the testing environment.
+ */
 @SpringBootTest
 class ServiceChallengeApplicationTests {
-
+/**
+     * This method is annotated with @Test to indicate that it is a test case.
+     * The test case ensures that the Spring context loads without errors.
+     */
     @Test
     void contextLoads() {
+        // If issues were loading the context, an exception would be thrown.
     }
 
 }
