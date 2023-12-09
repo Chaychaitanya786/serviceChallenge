57d12236ba5f457e7ffc0a35c162f000bc2aa253
Chaychaitanya786 <153323918+Chaychaitanya786@users.noreply.github.com>
2023-12-09 14:29:49 -0500

    Update AccountListService.java

--- src/main/java/com/challenge/servicechallenge/service/AccountListService.java
+++ src/main/java/com/challenge/servicechallenge/service/AccountListService.java
@@ -12,20 +12,42 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Service class for handling business logic related to accounts.
+ * Uses the @Service annotation to indicate that this class is a Spring service.
+ */
+
 @Service
 public class AccountListService {
     private final FileExtractService fileExtractService;
     private  final ObjectMapper objectMapper = new ObjectMapper();
+    /**
+       * Constructor for injecting the FileExtractService dependency.
+       */
+    
     public AccountListService(FileExtractService fileExtractService) {
         super();
         this.fileExtractService=fileExtractService;
     }
+
+    /**
+       * Retrieves a list of all accounts along with the total balance.
+       *
+       * @return ListOfAccounts containing the list of accounts and the total balance.
+       */
+    
     public ListOfAccounts getAllAccounts() {
         JsonNode listAccounts = fileExtractService.extractFile("listOfAccounts", false);
         List<Account> lA = objectMapper.convertValue(listAccounts, new TypeReference<List<Account>>() {});
         return new ListOfAccounts(lA, lA.stream().map(Account::getBalance).reduce(0.0, Double::sum));
     }
-
+    /**
+       * Retrieves an account by its ID.
+       * @param id The ID of the account to retrieve.
+       * @return Account with the specified ID.
+       * @throws AccountNotFoundException If the account with the specified ID is not found.
+       */
+    
     public Account getAccountByID(int id) {
         JsonNode listAccounts = fileExtractService.extractFile("listOfAccounts", false);
         Optional<Account> account = objectMapper.convertValue(listAccounts, new TypeReference<List<Account>>() {}).stream().filter(act -> act.getId() == id).findFirst();
