8ca1885627f5e6999a0996dc62ed50432d5ed2d6
Chaychaitanya786 <153323918+Chaychaitanya786@users.noreply.github.com>
2023-12-09 14:23:39 -0500

    Update FileServiceNotConfiguredException.java

--- src/main/java/com/challenge/servicechallenge/exceptionHandelling/FileServiceNotConfiguredException.java
+++ src/main/java/com/challenge/servicechallenge/exceptionHandelling/FileServiceNotConfiguredException.java
@@ -3,6 +3,12 @@ package com.challenge.servicechallenge.exceptionHandelling;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Custom exception class to represent a scenario where the file service type is not configured.
+ * The @ResponseStatus annotation is used to specify the HTTP status code (400 BAD REQUEST)
+ * and the reason for the exception when it is thrown.
+ */
+
 @ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "File Service Type not configured")
 public class FileServiceNotConfiguredException extends RuntimeException {
     public FileServiceNotConfiguredException() {
